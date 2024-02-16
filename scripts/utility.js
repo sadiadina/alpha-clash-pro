f045864bc8db6a324443a537d4912b894de9be85
sadiadina <sadiadina55@gmail.com>
2024-02-17 00:50:17 +0600

    created functional game

--- scripts/utility.js
+++ scripts/utility.js
@@ -13,6 +13,29 @@ function setBackgroundColorById(elementId){
 
 }
 
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueText = element.innerText;
+    const value = parseInt(elementValueText);
+    return value;
+}
+
+function setTextElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    element.innerText = value;
+}
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
 
 function getRandomAlphabet(){
     //get or create an alphabet array
